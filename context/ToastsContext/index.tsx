0cfd4dac471b8c043414b4069bdb0df671dbfb41
techsaavy8784 <blueskydev1205@gmail.com>
2024-05-31 09:17:28 +0800

    frontend and smart contract integration

---
+++ context/ToastsContext/index.tsx
@@ -0,0 +1,4 @@
+export * from './Provider'
+
+
+export { default as ToastListener } from './Listener'
---
+++ pages/_app.tsx
@@ -0,0 +1,38 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import Layout from '../components/Layout'
+import Providers from '../Providers'
+import store from '../store/store'
+import { WagmiConfig, createConfig } from 'wagmi'
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID,
+
+    // Required
+    appName: 'Your App Name',
+    // Optional
+    appDescription: 'Your App Description',
+    appUrl: 'https://family.co', // your app's url
+    appIcon: 'https://family.co/logo.png', // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+)
+
+function MyApp({ Component, pageProps }: AppProps) {
+  console.log('This is my App')
+  return (
+    <Layout title="Lending and Borrowing">
+      <WagmiConfig config={config}>
+        <ConnectKitProvider>
+          <Component {...pageProps} />
+        </ConnectKitProvider>
+      </WagmiConfig>
+    </Layout>
+  )
+}
+
+export default MyApp
